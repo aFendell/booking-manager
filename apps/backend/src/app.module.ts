7e09f3a62eb8c70cc13ec3a805cd1da63211ed1f
Assaf Fendell <fendell.assaf@gmail.com>
2023-12-26 16:02:03 +0200

    feat: docker db typorm config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,16 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: configService.get('DB_PORT'),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        entities: ['dist/**/*.entity.js'],
+        extra: {
+          charset: 'utf8mb4_unicode_ci',
+        },
+        synchronize: true,
+      }),
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'frontend', 'dist'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/backend/src/config.schema.ts
@@ -0,0 +1,9 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_NAME: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+});
